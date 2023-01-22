c4aaf0553faadece0b5981fd911f13cd2d9e4438
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2023-01-22 13:49:37 +0000

    Deploying to gh-pages from @ DidorchukAlexandr/goit-react-hw-08-phonebook@141a8ca46c4443f026a5fb2e7f56d30321c5df28 🚀

---
+++ static/js/806.b19895dc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[806],{3806:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var o=t(890),r=t(8870),c=t(184);function a(){return(0,c.jsxs)(o.Z,{variant:"h2",component:"h1",padding:"50px 24px 0",textAlign:"center",children:[(0,c.jsx)(r.Z,{component:"span",children:"\ud83d\udcf1"})," Phonebook welcome page"]})}}}]);
+//# sourceMappingURL=806.b19895dc.chunk.js.map
